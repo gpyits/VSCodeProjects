b748c2624d1ef2d39f39d02fa18d26d00609e35e
gpyits <gquagliettaits@gmail.com>
2024-04-26 21:19:06 +0200

    added pswd gen

--- Esercizi/Lezione 4+/pswd_generator.py
+++ Esercizi/Lezione 4+/pswd_generator.py
@@ -1,5 +1,13 @@
 # Create a function that generates a random password with a specified length and desired character types (lowercase letters, uppercase letters, numbers, symbols).
 # Allow the user to specify the password length and desired character types. Generate and return a random password that meets the user's criteria.
+import random
 
-def password_generator(password_length, character_types):
-    pass
+#very simplistic method, desired character types turned on by default
+def password_generator(password_length, want_lowercase=True, want_uppercase=True, want_numbers=True, want_symbols=True):
+    desired=['abcdefghijklmnopqrstuvwxyz' if want_lowercase else None, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if want_uppercase else None, 
+             '0123456789' if want_numbers else None, ',.-#+^\'\\!"£$%&/()=?[]@_:;<>|' if want_symbols else None]
+    desired.remove(None) if None in desired else desired
+    password=''.join(desired)
+    return ''.join(random.sample(password, password_length))
+
+print(password_generator(30))
