7ac3bd2708055de28d4dde6bab812e3771a6ba3a
gpyits <gquagliettaits@gmail.com>
2024-05-01 23:37:20 +0200

    added flag bubblesort and iterative fibonacci

--- Lezioni/bubblesort.py
+++ Lezioni/bubblesort.py
@@ -1,5 +1,5 @@
 import time
-a=[i for i in range(1, (10**4)+1)]
+a=[i for i in range(1, (10**2)+1)]
 
 def bubble_sort(list):
     start=time.time()
@@ -9,7 +9,7 @@ def bubble_sort(list):
                 dummy=list[j]
                 list[j]=list[j+1]
                 list[j+1]=dummy
-    return list, time.time()-start
+    return list, f'{time.time()-start} seconds'
 
 def bubble_sort_improved(list):
     start=time.time()
@@ -19,11 +19,21 @@ def bubble_sort_improved(list):
                 dummy=list[j]
                 list[j]=list[j+1]
                 list[j+1]=dummy
-    return list, time.time()-start
+    return list, f'{time.time()-start} seconds'
 
-#see pdf on moodle
 def flag_bubble_sort(list):
-    pass
+    start=time.time()
+    for i in range(len(list)):
+        swap_flag=False
+        for j in range(len(list)-i-1):
+            if list[j]>list[j+1]:
+                swap_flag=True
+                dummy=list[j]
+                list[j]=list[j+1]
+                list[j+1]=dummy
+    if swap_flag==False:
+        return list, f'{time.time()-start} seconds'
 
 print(bubble_sort(a[::-1]))
 print(bubble_sort_improved(a[::-1]))
+print(flag_bubble_sort(a[::-1]))
