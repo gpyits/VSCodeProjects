a943163632e862d91018b452b67698e0f6894a00
gpyits <gquagliettaits@gmail.com>
2024-05-02 10:28:08 +0200

    added 4-7

--- Esercizi/Lezione 4 - aggiuntivi/4.7 - roman_conversion.py
+++ Esercizi/Lezione 4 - aggiuntivi/4.7 - roman_conversion.py
@@ -2,25 +2,23 @@
 # Handle numbers from 1 to 3999.
 # Use a combination of string manipulation and conditional statements to build the Roman numeral.
 
-#1=I, 5=V, 10=X
-#50=L, 100=C
-#500=M, 1000=M
-#special numbers: 4, 9, 40, 90, 400, 900 
-
-#ones, tens, hundreds, thousands=number%10, (number%100)//10, (number%1000)//100, number//1000
 def roman_numeral_conversion(number: int)->str:
-    #because of how conversion is handled, I'd need to remove the 4s and 9s and include the directly into the conversion instead as if 4/9 else
-    conversion_table={1:'I', 4:'IV', 5:'V', 9:'IX', 10:'X', 40:'XL', 50:'L', 90:'XC', 100:'C', 400:'CD', 500:'M', 900:'CM', 1000:'M'}
+    #conveniently arranged conversion table
+    conversion_table={1:['I', 'IV', 'V', 'IX'], 2:['X', 'XL', 'L', 'XC'], 3:['C', 'CD', 'D', 'CM'], 4:'M'}
+    converted_number=''
     #checks if number is within allowed range
     if number not in range(1, 4000):
         return False
-    #number values as their decimal place value
+    #number values as their decimal positional value: ones, tens, hundreds, thousands
     pos_values=[number%10, (number%100)//10, (number%1000)//100, number//1000]
-    for k, v in conversion_table.items():
-        for i in pos_values:
-            #check for nearest "5" (make dummy first key for values les than 5) item to the left in the keys,
-            #then convert it to the value*(number-5 of the 1 value of that position, e.g. I, X, C, M)
-            pass
-            
+    #position flag
+    position=1
+    for i in pos_values:
+        #converts based on the conversion table dictionary
+        i=conversion_table[position][0]*i if i<4 else conversion_table[position][1] if i==4 else conversion_table[position][2] if i==5 else conversion_table[position][2]+conversion_table[position][0]*(i-5) if i<9 else conversion_table[position][-1] if i==9 else 'M' if position==4 else None
+        converted_number+=i[::-1]
+        position+=1
+    return converted_number[::-1]
 
-roman_numeral_conversion(3333)
+for r in range(1, 4001):
+    print(f'{r}: ', roman_numeral_conversion(r))
