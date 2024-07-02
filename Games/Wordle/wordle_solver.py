37e2edb3a76eabc4ed74670dfb2d95ba28dad65b
gpyits <gquagliettaits@gmail.com>
2024-07-02 09:16:46 +0200

    .

--- Games/Wordle/wordle_solver.py
+++ Games/Wordle/wordle_solver.py
@@ -3,6 +3,7 @@
 #Input all the uppercase yellow letters found in the current game in this format ->  yellow_letters='letter1letter2letter3' e.g. 'SDE'
 #Input all the uppercase black letters found in the current game in this format ->  black_letters='letter1letter2letter3' e.g. 'ANGOPDH'
 import re
+
 def wordle_solver(green_letters: list[tuple[str, int]], yellow_letters: str, black_letters: str) -> list[str]:
     with open('Games/Wordle/wordle_words_eng.txt', 'r') as f:
         lines=f.readlines()
@@ -15,4 +16,4 @@ def wordle_solver(green_letters: list[tuple[str, int]], yellow_letters: str, bla
         result=list(set(word for word in [word [0] for word in [re.findall(green_letters, line.strip('\n')) for line in lines] if word] if all(letter in word for letter in yellow_letters)))
         return sorted([word for word in result if all(yellow_count[letter]=={k:word.count(k) for k in set(word)}[letter] for letter in yellow_letters) and all(letter not in word for letter in black_letters)])
 
-print(wordle_solver('..A.E', 'D', 'CHIRMOUNTLSVWL'))
+print(wordle_solver('..L..', 'AIN', 'CHRMOUTBES'))
