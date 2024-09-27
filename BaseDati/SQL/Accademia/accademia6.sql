7341daea9ddce4d8c0582c662364ab64870b57bd
gpyits <gquagliettaits@gmai.com>
2024-09-27 13:57:29 +0200

    added sql queries

---
+++ BaseDati/SQL/Accademia/accademia6.sql
@@ -0,0 +1,34 @@
+-- 1. Quanti sono gli strutturati di ogni fascia?
+select
+
+-- 2. Quanti sono gli strutturati con stipendio ≥ 40000?
+
+
+-- 3. Quanti sono i progetti già finiti che superano il budget di 50000?
+
+
+-- 4. Qual è la media, il massimo e il minimo delle ore delle attività relative al progetto
+-- ‘Pegasus’ ?
+
+
+-- 5. Quali sono le medie, i massimi e i minimi delle ore giornaliere dedicate al progetto
+-- ‘Pegasus’ da ogni singolo docente?
+
+
+-- 6. Qual è il numero totale di ore dedicate alla didattica da ogni docente?
+
+
+-- 7. Qual è la media, il massimo e il minimo degli stipendi dei ricercatori?
+
+
+-- 8. Quali sono le medie, i massimi e i minimi degli stipendi dei ricercatori, dei professori
+-- associati e dei professori ordinari?
+
+
+-- 9. Quante ore ‘Ginevra Riva’ ha dedicato ad ogni progetto nel quale ha lavorato?
+
+
+-- 10. Qual è il nome dei progetti su cui lavorano più di due strutturati?
+
+
+-- 11. Quali sono i professori associati che hanno lavorato su più di un progetto?
---
+++ BaseDati/SQL/Cielo/cielo2.sql
@@ -0,0 +1,43 @@
+-- 1. Quante sono le compagnie che operano (sia in arrivo che in partenza) nei diversi
+-- aeroporti?
+
+
+-- 2. Quanti sono i voli che partono dall’aeroporto ‘HTR’ e hanno una durata di almeno
+-- 100 minuti?
+
+
+-- 3. Quanti sono gli aeroporti sui quali opera la compagnia ‘Apitalia’, per ogni nazione
+-- nella quale opera?
+
+
+-- 4. Qual è la media, il massimo e il minimo della durata dei voli effettuati dalla
+-- compagnia ‘MagicFly’ ?
+
+
+-- 5. Qual è l’anno di fondazione della compagnia più vecchia che opera in ognuno degli
+-- aeroporti?
+
+
+-- 6. Quante sono le nazioni (diverse) raggiungibili da ogni nazione tramite uno o più
+-- voli?
+
+
+-- 7. Qual è la durata media dei voli che partono da ognuno degli aeroporti?
+
+
+-- 8. Qual è la durata complessiva dei voli operati da ognuna delle compagnie fondate
+-- a partire dal 1950?
+
+
+-- 9. Quali sono gli aeroporti nei quali operano esattamente due compagnie?
+
+
+-- 10. Quali sono le città con almeno due aeroporti?
+
+
+-- 11. Qual è il nome delle compagnie i cui voli hanno una durata media maggiore di 6
+-- ore?
+
+
+-- 12. Qual è il nome delle compagnie i cui voli hanno tutti una durata maggiore di 100
+-- minuti?
