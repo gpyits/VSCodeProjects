752159b5592a06a28f953491245d0552ef964c5c
gpyits <gquagliettaits@gmail.com>
2024-11-08 13:37:55 +0100

    added accademia7

---
+++ BaseDati/SQL/Accademia/accademia7.sql
@@ -0,0 +1,27 @@
+-- Qual è media e deviazione standard degli stipendi per ogni categoria di strutturati?
+
+
+-- Quali sono i ricercatori (tutti gli attributi) con uno stipendio superiore alla media
+-- della loro categoria?
+
+
+-- Per ogni categoria di strutturati quante sono le persone con uno stipendio che
+-- differisce di al massimo una deviazione standard dalla media della loro categoria?
+
+
+-- Chi sono gli strutturati che hanno lavorato almeno 20 ore complessive in attività
+-- progettuali? Restituire tutti i loro dati e il numero di ore lavorate.
+
+
+-- Quali sono i progetti la cui durata è superiore alla media delle durate di tutti i
+-- progetti? Restituire nome dei progetti e loro durata in giorni.
+
+
+-- Quali sono i progetti terminati in data odierna che hanno avuto attività di tipo
+-- “Dimostrazione”? Restituire nome di ogni progetto e il numero complessivo delle
+-- ore dedicate a tali attività nel progetto.
+
+
+-- Quali sono i professori ordinari che hanno fatto più assenze per malattia del nu-
+-- mero di assenze medio per malattia dei professori associati? Restituire id, nome e
+-- cognome del professore e il numero di giorni di assenza per malattia.
